30c0eeb96a3025d2f15cbd169d88a30bfdcd3529
Amit Pritam <amitpritam1989@gmail.com>
2025-04-22 10:59:18 +0530

    string changes

--- app/views.py
+++ app/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def amit(request):
-    return HttpResponse('<h1><marquee>My name is Barry Allen and i am the fastest man alive</marquee></h1>')
+    return HttpResponse('<h1><marquee direction=right>My name is Barry Allen and i am the fastest man alive</marquee></h1> <h1><marquee>Long Conversation, we loose track of time</marquee></h1>')
